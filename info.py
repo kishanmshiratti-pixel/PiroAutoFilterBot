eaf82370be557d628f4f06441168c6b369f45bcb
kishanmshiratti-pixel <kishanmshiratti@gmail.com>
2026-05-19 13:07:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,9 +18,9 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+API_ID = int(environ.get('API_ID', '24799187'))
+API_HASH = environ.get('API_HASH', '4baef40b8f24b66a25ccd232861186ed')
+BOT_TOKEN = environ.get('BOT_TOKEN', '8801108307:AAHnJmdf5nR7p4Wth56WL2Nqp6BkWUXENIQ')
 PORT = environ.get("PORT", "8080")
 
 # Bot settings
@@ -36,7 +36,7 @@ MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/72dff2b65352ba85d
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/2a888a370f479f4338f7c.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '777000').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
@@ -51,8 +51,8 @@ SUPPORT_CHAT_ID = -1001792675255
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "PIRO")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://kishan:22996@epiccastle.w4lsygq.mongodb.net/?appName=EpicCastle")
+DATABASE_NAME = environ.get('DATABASE_NAME', "EpicCastle")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
 # Others
